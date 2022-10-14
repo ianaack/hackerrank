5428c8903880a34fd2721bf7ef0dea9436af4baf
ianaack <ianaack@gmail.com>
2022-10-13 20:55:01 -0400

    Day 12 of 30: Inheritance

---
+++ 30 Days of Code/day12of30.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var _input = "";
+var _index = 0;
+process.stdin.on("data", (data) => {
+	_input += data;
+});
+process.stdin.on("end", () => {
+	_input = _input.split(new RegExp("[ \n]+"));
+	main();
+});
+function read() {
+	return _input[_index++];
+}
+
+/**** Ignore above this line. ****/
+
+class Person {
+	constructor(firstName, lastName, identification) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+
+	printPerson() {
+		console.log(
+			"Name: " +
+				this.lastName +
+				", " +
+				this.firstName +
+				"\nID: " +
+				this.idNumber
+		);
+	}
+}
+
+class Student extends Person {
+	/*
+	 *   Class Constructor
+	 *
+	 *   @param firstName - A string denoting the Person's first name.
+	 *   @param lastName - A string denoting the Person's last name.
+	 *   @param id - An integer denoting the Person's ID number.
+	 *   @param scores - An array of integers denoting the Person's test scores.
+	 */
+	// Write your constructor here
+	constructor(firstName, lastName, idNumber, scores) {
+		super(firstName, lastName, idNumber);
+		this.scores = scores;
+	}
+
+	/*
+	 *   Method Name: calculate
+	 *   @return A character denoting the grade.
+	 */
+	// Write your method here
+	calculate() {
+		let sum = 0;
+		let avg = 0;
+		let grade = " ";
+
+		for (let i = 0; i < this.scores.length; i++) {
+			sum = sum + parseInt(this.scores[i]);
+		}
+
+		if (this.scores.length != 0) avg = sum / this.scores.length;
+
+		if (avg >= 90 && avg <= 100) grade = "O";
+		else if (avg >= 80 && avg < 90) grade = "E";
+		else if (avg >= 70 && avg < 80) grade = "A";
+		else if (avg >= 55 && avg < 70) grade = "P";
+		else if (avg >= 40 && avg < 55) grade = "D";
+		else if (avg < 40) grade = "T";
+
+		return grade;
+	}
+}
+
+function main() {
+	let firstName = read();
+	let lastName = read();
+	let id = +read();
+	let numScores = +read();
+	let testScores = new Array(numScores);
+
+	for (var i = 0; i < numScores; i++) {
+		testScores[i] = +read();
+	}
+
+	let s = new Student(firstName, lastName, id, testScores);
+	s.printPerson();
+	s.calculate();
+	console.log("Grade: " + s.calculate());
+}
